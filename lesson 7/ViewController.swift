291010801e52c69e804a8807aea0d63db9a5e045
Alexandr <74298908+Alexandr205@users.noreply.github.com>
2022-07-28 20:20:59 +0600

    Initial Commit

---
+++ lesson 7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lesson 7
+//
+//  Created by Александр Калашников on 28/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
